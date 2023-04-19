5b704846cc7bdbb1aac6b9924063ebd8f58a7c9b
terizah <88724711+terizah@users.noreply.github.com>
2023-04-19 19:32:37 +1000

    Update Accommodation.java

--- Accommodation.java
+++ Accommodation.java
@@ -7,4 +7,23 @@ public class Accommodation {
   private double price;
   
  public Accommodation(int accommodation, String type, double price) {
-  this.accommodationID = a
+  this.accommodationID = accommodationID;
+  this.type = type;
+  this.price = price;
+ }
+
+ public int getAccommodationID () {
+  return AccommodationID;
+ }
+ 
+ public String getType () {
+  return type;
+ }
+
+  public double getPrice () {
+    return price;
+ }
+}
+
+  
+
