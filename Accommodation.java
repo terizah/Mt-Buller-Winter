2f5519401ecb2844c938a6f89acfba99e5f435f3
terizah <88724711+terizah@users.noreply.github.com>
2023-04-20 02:12:41 +1000

    Update Accommodation.java

--- Accommodation.java
+++ Accommodation.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Accommodation {
   private int accommodationID;
   private String type;
-  private double price;
+  private double pricePerNight;
   private boolean availability = true;
   static int nextID = 1;
   
- public Accommodation(String type, double price) {
+ public Accommodation(String type, double pricePerNight) {
   this.type = type;
-  this.price = price;
+  this.pricePerNight = pricePerNight;
   accommodationID = nnextID++;
  }
 
@@ -22,8 +22,8 @@ public class Accommodation {
   return type;
  }
 
-  public double getPrice () {
-    return price;
+  public double getPricePerNgith () {
+    return pricePerNight;
  }
  
  public boolean getAvailability () {
@@ -34,8 +34,8 @@ public void setType (String type) {
   this.type = type;
  }
  
- public void setPrice (double price) {
-  this.price = price;
+ public void setPricePerNight (double pricePerNight) {
+  this.pricePerNight = pricePerNight;
  }
   
  public void setAvailability (boolean availability) {
@@ -43,9 +43,29 @@ public void setType (String type) {
  }
  
  public String toString () {
-  return "Accommodation ID: " + accommodationID + ", type: + type + ", price: " + price + ", available? " + availability;
+  return "Accommodation ID: " + accommodationID + ", type: + type + ", pricePerNight: " + pricePerNight + ", available? " + availability;
  }
 }
+
+public class Main {
+  public static void main(String[] args) {
+Arraylist<Accommodation> accommodations = new ArrayList<Accommodation> ();
+  accommodations.add(new Accommodation("001", "Hotel", 150, true));
+  accommodations.add(new Accommodation("002", "Inn", 100, true));
+  accommodations.add(new Accommodation("003", "Apartment", 200, true));
+  accommodations.add(new Accommodation("004", "Cabin", 150, true));
+  accommodations.add(new Accommodation("005", "Lodge", 250, true));
+  accommodations.add(new Accommodation("006", "Campgroud", 100, true));
+  accommodations.add(new Accommodation("007", "Motel", 110, true));
+  accommodations.add(new Accommodation("008", "Cottage", 150, true));
+  accommodations.add(new Accommodation("009", "Resort", 300, true));
+  accommodations.add(new Accommodation("001", "Suite", 400, true));
+
+System.out.println(accommodations);
+ }
+}
+
+
 
 
   
