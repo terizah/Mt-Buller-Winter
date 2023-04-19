776cf8d9c6806fa8ebbcf38a8a9188cb4d7cca99
terizah <88724711+terizah@users.noreply.github.com>
2023-04-19 19:44:42 +1000

    Update Accommodation.java

--- Accommodation.java
+++ Accommodation.java
@@ -5,11 +5,13 @@ public class Accommodation {
   private int accommodationID;
   private String type;
   private double price;
+  private boolean availability = true;
+  static int nextID = 1;
   
- public Accommodation(int accommodation, String type, double price) {
-  this.accommodationID = accommodationID;
+ public Accommodation(String type, double price) {
   this.type = type;
   this.price = price;
+  accommodationID = nnextID++;
  }
 
  public int getAccommodationID () {
@@ -23,7 +25,28 @@ public class Accommodation {
   public double getPrice () {
     return price;
  }
+ 
+ public boolean getAvailability () {
+  return availability;
 }
+
+public void setType (String type) {
+  this.type = type;
+ }
+ 
+ public void setPrice (double price) {
+  this.price = price;
+ }
+  
+ public void setAvailability (boolean availability) {
+  this.availability = availability;
+ }
+ 
+ public String toString () {
+  return "Accommodation ID: " + accommodationID + ", type: + type + ", price: " + price + ", available? " + availability;
+ }
+}
+
 
   
 
