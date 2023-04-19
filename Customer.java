6b294ea261b9a38bb8b3a78425ca4997b18e74e9
terizah <88724711+terizah@users.noreply.github.com>
2023-04-19 20:20:21 +1000

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,17 @@
+public class Customer {
 
+  private int custId
+  private String name;
+  private int age;
+  private String skiingLevel; 
+  static int nextID = 100;
+  
+public customer(String name, int age, String skiingLevel) {
+  this.name = name;
+  this.age = age;
+  this.skiingLevel = skiingLevel;
+ }
+ 
+  
+ 
+ 
