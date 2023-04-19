61f84e94f66fc49962075bbd7fdde21b45aa0475
terizah <88724711+terizah@users.noreply.github.com>
2023-04-19 19:23:41 +1000

    Create Accommodation.java

--- Accommodation.java
+++ Accommodation.java
@@ -1,1 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Accommodation {
+  private int accommodationID;
+  private String type;
+  private double price;
+  
+ public Accommodation(int accommodation, String type, double price) {
+  this.accommodationID = a
