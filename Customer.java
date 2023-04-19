56bce8a85db2f716383f80da2c3b1357158291eb
terizah <88724711+terizah@users.noreply.github.com>
2023-04-19 20:53:22 +1000

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,5 +1,4 @@
 public class Customer {
-
   private int custId
   private String name;
   private int age;
@@ -10,7 +9,35 @@ public customer(String name, int age, String skiingLevel) {
   this.name = name;
   this.age = age;
   this.skiingLevel = skiingLevel;
+  custId = nnextID++;
  }
+ 
+public int getCustId () {
+  return custId;
+}
+
+public String getName () {
+  return name;
+}
+
+public int getAge () {
+  return age;
+}
+
+public String getSkiingLevel () {
+  return skiingLevel;
+}
+
+public void setName (String name) {
+  this.name = name;
+}
+
+public void addCustomer (String name, int age, String skiingLevel) {
+  
+  Customer newCustomer = new Customer(name, age, skiingLevel);
+  customerList.add(newCustomer);
+
+
  
   
  
