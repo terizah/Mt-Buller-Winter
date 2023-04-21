fd7d499de584414ffef89e6e395990aa2c22f33b
terizah <88724711+terizah@users.noreply.github.com>
2023-04-21 14:31:49 +1000

    Update Lesson.java

--- Lesson.java
+++ Lesson.java
@@ -1,1 +1,26 @@
+public class Lesson {
+  private String level;
+  private double fee;
+ 
+public Lesson (String level, double fee) {
+  this.level = level
+  this.fee = fee;
+}
+
+public String getLevel () {
+  return level;
+}
+
+public double getFee () {
+  return fee;
+}
+
+public void setLevel (String lee) {
+  this.level = level;
+}
+
+public void setFee (double fee) {
+  this.fee + fee
+ }
+}
 
