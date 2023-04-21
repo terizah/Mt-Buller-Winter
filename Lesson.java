013396c93463be8f4f0e028082959a0e73d34df1
terizah <88724711+terizah@users.noreply.github.com>
2023-04-21 15:01:03 +1000

    Update Lesson.java

--- Lesson.java
+++ Lesson.java
@@ -24,3 +24,19 @@ public void setFee (double fee) {
  }
 }
 
+public void addLessons(Package package, intnumLessons, String level) {
+  
+  double fee = 0.0;
+  if (level.equals("beginner")) {
+    fee = 25.0;
+  } else if (level.equals("intermediate")) {
+    fee = 20.0;
+  } else if (level.equals("expert")) {
+    fee = 15.0;
+  }
+    Lesson lesson = new Lesson(level, fee);
+    
+    for (int i = o; i < numLessons; i++) {
+      package.addLesson(lesson);
+  }
+ }
