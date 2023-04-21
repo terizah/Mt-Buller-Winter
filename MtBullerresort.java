9d327796d4a95152d085ab78c07991b4b3336a9b
terizah <88724711+terizah@users.noreply.github.com>
2023-04-21 14:26:46 +1000

    Update MtBullerresort.java

--- MtBullerresort.java
+++ MtBullerresort.java
@@ -1,6 +1,25 @@
-public class Main {
-  public static void main(String[] args) {
-Arraylist<Accommodation> accommodations = new ArrayList<Accommodation> ();
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class MtBullerResort {
+ 
+ public static finial double Beginner_Lesson_Fee = 25.0;
+ public static finial double Intermediate_Lesson_Fee = 20.0;
+ public static final double Expert_Lesson_Fee = 15.0;
+ 
+ public static final double Full_Day_Lift_Pass_Price = 26.0;
+ public static final double Lift_Pass_5_Days_Disocunt = 0.1;
+ public static final double Lift_Pass_Season_Price = 200.0;
+ 
+ public static final String Customers_File_Name = "customer.txt";
+ public static final Packages_Flies_Name = 'packages.txt";
+ 
+ 
+puble static Arraylist<Accommodation> accommodations = new ArrayList<Accommodation> ();
+public static Scanner scanner = ner Scanner(System.in);
+public static void main(String[] args {
+
   accommodations.add(new Accommodation("001", "Hotel", 150, true));
   accommodations.add(new Accommodation("002", "Inn", 100, true));
   accommodations.add(new Accommodation("003", "Apartment", 200, true));
@@ -12,55 +31,58 @@ Arraylist<Accommodation> accommodations = new ArrayList<Accommodation> ();
   accommodations.add(new Accommodation("009", "Resort", 300, true));
   accommodations.add(new Accommodation("001", "Suite", 400, true));
 
-System.out.println(accommodations);
- }
-}
+pubic static ArrayList<Customer> customers = new ArrayList<Customer>();
+public static Scanner scanner = ner Scanner(System.in);
+public static void main(String[] args {
 
-2.
-ArrayList<Customer> customers = new ArrayList<Customer>();
-customers.add(new Customer("001", "John", "Beginner"));
-customers.add(new Customer("002", "Mary", "Intermediate"));
-customers.add(new Customer("003", "Tom", "Expert"));
+customers.add(new Customer("001", "Teresa", "Beginner"));
+customers.add(new Customer("002", "Randy", "Intermediate"));
+customers.add(new Customer("003", "Michelle", "Expert"));
+loadCustomerFromFile();
+loadPackagesFromFile();
 
-3.
-public static void main(String[] args) {
-    ArrayList<Accommodation> accommodations = new ArrayList<Accommodation>();
-    accommodations.add(new Accommodation("001", "Hotel", 200, true));
-    accommodations.add(new Accommodation("002", "Apartment", 150, true));
-    accommodations.add(new Accommodation("003", "Chalet", 300, true));
-    // add more accommodations here
+ int choice;
+ do {
+  displayMenu();
+  System.outprint("Enter you choice: ");
+  choice = scanner.nextInt();
+  scanner.nextLine();
+  switch (choice) :
+    case 1:
+      displayAccommodations();
+      break;
+     case 2: 
+      displayAvailableAccommdations();
+      break;
+     case 3:
+      addCustomer()
+      break;
+     case 4;
+      listCustomers();
+      breal;
+     case 5;
+      creatPackage();
+      break;
+     case 6;
+      addLiftPassToPackage();
+      break;
+     case 7;
+      addLessonFeesToPackage();
+      break;
+     case 8;
+      listPackages();
+      break;
+     case 9;
+      savePackagestoFile();
+      break;
+      case 10;
+        loadPackagesFromFile();
+        break;
+        case 11;
+        exit();
     
-    ArrayList<Customer> customers = new ArrayList<Customer>();
-    customers.add(new Customer("001", "John", "Beginner"));
-    customers.add(new Customer("002", "Mary", "Intermediate"));
-    customers.add(new Customer("003", "Tom", "Expert"));
-    
-    MtBullerResort resort = new MtBullerResort(accommodations, customers);
-    // start your application here
-}
+   
 
-4.
-Scanner scanner = new Scanner(System.in);
-System.out.println("Enter the customer ID: ");
-String customerId = scanner.nextLine();
 
-System.out.println("Enter the start date (in yyyy-MM-dd format): ");
-String startDateStr = scanner.nextLine();
-try {
-    LocalDate startDate = LocalDate.parse(startDateStr);
-} catch (DateTimeParseException e) {
-    System.out.println("Invalid date format. Please enter a date in yyyy-MM-dd format.");
-    // handle the exception here
-}
 
-System.out.println("Enter the duration (in days): ");
-int duration = scanner.nextInt();
-
-5.
-try {
-    // some code that may throw an exception
-} catch (Exception e) {
-    System.out.println("An error occurred: " + e.getMessage());
-    // handle the exception here
-}
 
