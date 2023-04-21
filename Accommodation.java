b6c4557b62e2c3e2075cbf5f8518392619328c43
terizah <88724711+terizah@users.noreply.github.com>
2023-04-21 15:01:58 +1000

    Update Accommodation.java

--- Accommodation.java
+++ Accommodation.java
@@ -46,27 +46,3 @@ public void setType (String type) {
   return "Accommodation ID: " + accommodationID + ", type: + type + ", pricePerNight: " + pricePerNight + ", available? " + availability;
  }
 }
-
-public class Main {
-  public static void main(String[] args) {
-Arraylist<Accommodation> accommodations = new ArrayList<Accommodation> ();
-  accommodations.add(new Accommodation("001", "Hotel", 150, true));
-  accommodations.add(new Accommodation("002", "Inn", 100, true));
-  accommodations.add(new Accommodation("003", "Apartment", 200, true));
-  accommodations.add(new Accommodation("004", "Cabin", 150, true));
-  accommodations.add(new Accommodation("005", "Lodge", 250, true));
-  accommodations.add(new Accommodation("006", "Campgroud", 100, true));
-  accommodations.add(new Accommodation("007", "Motel", 110, true));
-  accommodations.add(new Accommodation("008", "Cottage", 150, true));
-  accommodations.add(new Accommodation("009", "Resort", 300, true));
-  accommodations.add(new Accommodation("001", "Suite", 400, true));
-
-System.out.println(accommodations);
- }
-}
-
-
-
-
-  
-
